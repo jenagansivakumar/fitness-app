417ea98944c7d56114d10b1b1a98bbe6799d6e4a
Jena <jenagansivakumar@gmail.com>
2024-02-21 16:09:11 +0100

    setup ready, moving to navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,18 @@ import { Box } from "@mui/material";
 import ExerciseDetail from "./pages/ExerciseDetail";
 import { Navbar } from "./components/Navbar";
 import Home from "./pages/Home";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <Router>
-      <Box width="400">
+      <Box width="400" sx={{ width: { xl: "1488px" } }} m="auto">
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/exercise/:id" element={<ExerciseDetail />} />
         </Routes>
+        <Footer />
       </Box>
     </Router>
   );
--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const ExerciseDetail = () => {
   return <div>ExerciseDetail</div>;
 };
