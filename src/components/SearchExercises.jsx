53db14e4e8fa243007986ec8faa5e5bbe737f683
Jena <jenagansivakumar@gmail.com>
2024-02-21 18:23:36 +0100

    imported everything needed for SearchExercises component

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 
 export default function SearchExercises() {
   return <div>SearchExercises</div>;
