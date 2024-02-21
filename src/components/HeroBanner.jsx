26290309bf10ab265568cbbbb937dc976d60fe60
Jena <jenagansivakumar@gmail.com>
2024-02-21 16:36:15 +0100

    created slogan

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
 
 const HeroBanner = () => {
-  return <div>HeroBanner</div>;
+  return (
+    <Box
+      sx={{
+        mt: { lg: "212px", xs: "70px" },
+        ml: { sm: "50px" },
+      }}
+      position="relative"
+      p="20px"
+    >
+      <Typography color="#FF2625" fontWeight="600" fontSize="26px">
+        {" "}
+        Fitness Club
+      </Typography>
+      <Typography>
+        No Nonsense <br /> Results!
+      </Typography>
+      <Typography>
+        Check out the most efficient way to achieve your goals
+      </Typography>
+    </Box>
+  );
 };
 
 export default HeroBanner;
