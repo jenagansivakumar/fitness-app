1bd1351fde79b190179290ce07f83e086f6da8a2
Jena <jenagansivakumar@gmail.com>
2024-02-26 14:39:32 +0100

    mapped searched exercises

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -1,11 +1,27 @@
 import { useEffect, useState } from "react";
-import { Pagination } from "@mui/material/Pagination";
+import { Pagination } from "@mui/material";
 import { Box, Stack, Typography } from "@mui/material";
+import { exerciseOptions, fetchData } from "../utils/fetchData";
 
-useEffect;
-
-const Exercises = () => {
-  return <div>Exercises</div>;
+const Exercises = ({ exercises, setExercises, bodyPart }) => {
+  console.log(exercises);
+  return (
+    <Box id="exercises" sx={{ mt: "110px" }} mt="50px" p="20px">
+      <Typography variant="h3" mb="46px">
+        Showing Results
+      </Typography>
+      <Stack
+        direction="row"
+        sx={{ gap: { lg: "110px", xs: "50px" } }}
+        flexWrap="wrap"
+        justifyContent="center"
+      >
+        {exercises.map((exercise, index) => (
+          <p key={index}>{exercise.name}</p>
+        ))}
+      </Stack>
+    </Box>
+  );
 };
 
 export default Exercises;
