aeac2ecbb32663e2c79c700625f73cf69be4f4a8
Jena <jenagansivakumar@gmail.com>
2024-02-26 13:04:59 +0100

    imported all the libraries into Exercise.jsx

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -1,3 +1,9 @@
+import { useEffect, useState } from "react";
+import { Pagination } from "@mui/material/Pagination";
+import { Box, Stack, Typography } from "@mui/material";
+
+useEffect;
+
 const Exercises = () => {
   return <div>Exercises</div>;
 };
