b84463e9f93fb698f558f442740794b702580c96
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2024-03-04 18:41:12 +0100

    added scrollTo property

--- src/components/BodyPart.jsx
+++ src/components/BodyPart.jsx
@@ -20,6 +20,7 @@ const BodyPart = ({ item, setBodyPart, bodyPart }) => {
       }}
       onclick={() => {
         setBodyPart(item)
+        window.scrollTo({top: 1800, left: 100, behavior: "smooth"})
       }}
     >
       <img src={Icon} alt="dumbell" style={{ width: "40px", height: "40px" }} />
