638a0c1acc8a87d6ddc8934a85b1492e1ddf9c35
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2024-03-05 15:34:23 +0100

    gifs are now showing

--- src/components/ExerciseCard.jsx
+++ src/components/ExerciseCard.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { Button, Stack, Typography } from '@mui/material'
 
-export const ExerciseCard = () => {
+export const ExerciseCard = ({exercise}) => {
   return (
-    <div>ExerciseCard</div>
+    <Link className='exercise-card' to={ `/exercise/${exercise.id}`}>
+        <img src={exercise.gifUrl} alt={exercise.name} loading="lazy" />
+    </Link>
   )
 }
