237f962bd4ddc7d731050a1284a46f6bd07380bf
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2024-03-06 16:19:23 +0100

    created usestate for current page and a paginate function

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -7,6 +7,12 @@ import { ExerciseCard } from "./ExerciseCard";
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
   const [currentPage, setCurrentPage] = useState(1)
   const exercisesPerPage = 9
+
+  const paginate = (e, value) => {
+    setCurrentPage(value)
+    window.scrollTo({top: 1800, behavior: "smooth"})
+
+  }
   return (
     <Box id="exercises" sx={{ mt: "110px" }} mt="50px" p="20px">
       <Typography variant="h3" mb="46px">
@@ -27,7 +33,7 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
           <Pagination color="standard" shape="rounded" defaultPage={1} count={
             Math.ceil(exercises.length / exercisesPerPage )}
             page={currentPage}
-            onChange={paginate}
+            onChange={ paginate}
             size="large"
             />
         )}
