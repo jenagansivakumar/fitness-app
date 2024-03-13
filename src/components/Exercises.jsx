bf8fd1ad69464020c1ada45fc2b8c62c9b155de0
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2024-03-13 18:09:04 +0100

    changed colours on Exercises

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -10,7 +10,6 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
 
   const indexOfLastExercise = currentPage * exercisesPerPage;
   const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
-  // Check if exercises is an array before using slice
   const currentExercises = Array.isArray(exercises) ? exercises.slice(indexOfFirstExercise, indexOfLastExercise) : [];
 
   const paginate = (e, value) => {
@@ -22,26 +21,25 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
     const fetchExercisesData = async () => {
       try {
         let exercisesData = [];
-  
+
         if (bodyPart === 'all') {
           exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
         } else {
           exercisesData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`, exerciseOptions);
         }
-  
+
         setExercises(exercisesData);
       } catch (error) {
         console.error('Error fetching exercises:', error);
-       
       }
     };
-  
+
     fetchExercisesData();
   }, [bodyPart]);
 
   return (
     <Box id="exercises" sx={{ mt: "110px" }} mt="50px" p="20px">
-      <Typography variant="h3" mb="46px">
+      <Typography variant="h3" mb="46px" color="#bfbfbf">
         Showing Results
       </Typography>
       <Stack
